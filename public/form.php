daf1fa73c96b3e62d8554e2ee54662dca59c0966
001matt <pringuez.matthieu@gmail.com>
2015-01-27 09:18:12 +0100

    merge after commit

--- public/form.php
+++ public/form.php
@@ -1,4 +1,17 @@
-<?php include'header.php'?>
+<?php 
+require_once '../application/init.php';
+
+use todo\DbTable\Tache;
+
+if(isset($_GET['id'])){
+    $id =(int) $_GET['id'];
+    $crudTache = new Tache($connection);
+    $taches = $crudTache->findById($id);
+}else{
+    $id = null;
+}
+
+include'header.php'?>
 
 <div class="panel panel-info formulaire">
     <div class="panel-heading">
@@ -9,42 +22,42 @@
             <div class="form-group">
                 <label for="title" class="col-sm-2 control-label">Titre de la tâche</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="title" placeholder="Titre de la tâche">
+                    <input type="text" class="form-control" name="title" value="<?= empty($id) ? '' : $taches->getTitre() ;?>" placeholder="Titre de la tâche">
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="description" class="col-sm-2 control-label">Description de la tâche</label>
                 <div class="col-sm-10">
-                    <textarea type="textarea" rows="3" class="form-control" name="description" placeholder="Description de la tâche"></textarea>
+                    <textarea type="textarea" rows="3" class="form-control" name="description" placeholder="Description de la tâche"><?= empty($id) ? '' : $taches->getDescription(); ?></textarea>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="echeance" class="col-sm-2 control-label">Echéance</label>
                 <div class="col-sm-10">
-                    <input type="date" class="form-control" name="echeance" >
+                    <input type="date" class="form-control"  value="<?= empty($id) ? '' : $taches->getEcheance() ;?>" name="echeance" >
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="echeance" class="col-sm-2 control-label">Temps prévisionel</label>
                 <div class="col-sm-10">
-                    <input type="time" class="form-control" name="echeance" >
+                    <input type="time" class="form-control"  value="<?= empty($id) ? '' : $taches->getTimeRealisation() ;?>" name="echeance" >
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="echeance" class="col-sm-2 control-label">Utilisateur</label>
                 <div class="col-sm-10">
-                    <select name="user" id="" class="form-control">
-                        <option value="user1">Prisca</option>
-                        <option value="user1">Matthieu</option>
-                        <option value="user1">Murielle</option>
+                    <select name="user" id="" class="form-control" multiple>
+                        <?php foreach ($taches->getUsers() as $user) : ?>
+                        <option value="user1"><?= $user->getName().' '.$user->getFirstname() ;?></option>
+                        <?php endforeach;?>
                     </select>
                 </div>
             </div>
-
+            <input type="hidden" value="<?= $id; ?>">
             <div class="form-group">
                 <div class="col-sm-offset-9 col-sm-10">
                     <button type="submit" class="btn btn-primary">Créer</button>
