c022afe790356992720772d007fc207e8941ee41
lepriss <prisca.jufre@gmail.com>
2015-01-27 09:48:37 +0100

    top-bar login enlever

--- public/login.php
+++ public/login.php
@@ -1,6 +1,31 @@
-<?php include 'header.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>projet</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <style>
+        body {
+            padding-top: 60px;
+            padding-bottom: 40px;
+        }
+    </style>
 
+    <link rel="stylesheet" href="css/main.css">
 
+    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <main class="container">
             <div class="login">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
@@ -29,6 +54,7 @@
                     </div>
                 </div>
             </div>
-
-
+    </main>
+</body>
+</html>
 
