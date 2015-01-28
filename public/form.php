ae7f31e51936b06a98f24337bf22dee81d02ce64
lepriss <prisca.jufre@gmail.com>
2015-01-28 09:58:48 +0100

    modification form

--- public/form.php
+++ public/form.php
@@ -20,80 +20,81 @@ include 'header.php'?>
         <h3>Formulaire de création et de modification de tâches</h3>
     </div>
     <div class="panel-body">
-        <form class="form-horizontal" action="../application/controller/ctrlTache.php" methode="POST">
+        <form action="../application/controller/ctrlTache.php" methode="POST">
             <div class="form-group">
-                <label for="title" class="col-sm-2 control-label">Titre de la tâche</label>
-                <div class="col-sm-10">
+                <label for="title">Titre de la tâche</label>
+
                     <input type="text" class="form-control" name="titre" id="titre" value="<?= empty($id) ? '' : $taches->getTitre() ;?>" placeholder="Titre de la tâche">
-                </div>
+
 
             <div class="form-group">
-                <label for="description" class="col-sm-2 control-label">Description de la tâche</label>
-                <div class="col-sm-10">
+                <label for="description">Description de la tâche</label>
+
                     <textarea type="textarea" rows="3" class="form-control" name="description" id="description" placeholder="Description de la tâche"><?= empty($id) ? '' : $taches->getDescription(); ?></textarea>
-                </div>
+
 
             <div class="form-group">
-                <label for="echeance" class="col-sm-2 control-label">Echéance</label>
-                <div class="col-sm-10">
+                <label for="echeance">Echéance</label>
+
                     <input type="date" id="echeance" name="echeance" class="form-control"  value="<?= empty($id) ? '' : $taches->getEcheance() ;?>" name="echeance" >
-                </div>
+
 
             <div class="form-group">
-                <label for="echeance" class="col-sm-2 control-label">Temps prévisionel</label>
-                <div class="col-sm-10">
+                <label for="echeance" >Temps prévisionel</label>
+
                     <input type="time" class="form-control"  id="timeRealisation" name="timeRealisation" value="<?= empty($id) ? '' : $taches->getTimeRealisation() ;?>" name="echeance" >
-                </div>
+
             </div>
 
             <?php if(!empty($id)): ?>
             <div class="form-group">
-                <label for="user" class="col-sm-2 control-label">Utilisateur</label>
-                <div class="col-sm-10">
+                <label for="user">Utilisateur</label>
+
                     <select name="selectedUsers" id="selectUser" class="form-control" multiple>
                         <?php foreach ($taches->getUsers() as $user) : ?>
                         <option value="user1"><?= $user->getName().' '.$user->getFirstname() ;?></option>
                         <?php endforeach;?>
                     </select>
-                </div>
+
             </div>
             <?php endif; ?>
             
              <div class="form-group">
-                <label for="echeance" class="col-sm-2 control-label">Ajouter des utilisateurs</label>
-                <div class="col-sm-10">
+                <label for="echeance" >Ajouter des utilisateurs</label>
+
                     <select name="addUser" id="addUser" class="form-control" multiple>
                         <?php foreach ($users as $user) : ?>
                         <option value="user1"><?= $user->getName().' '.$user->getFirstname() ;?></option>
                         <?php endforeach;?>
                     </select>
-                </div>
+
             </div>
             
             <div class="form-group">
-                <label for="echeance" class="col-sm-2 control-label">Statut</label>
-                <div class="col-sm-10">
+                <label for="echeance">Statut</label>
+
                     <select name="statut" id="statut" class="form-control">
                         
                         <?php foreach ($taches->getStatutToString() as $key => $value) : ;?>
                             <option value="<?php echo $key; ?>"><?= $value; ?></option>
                         <?php endforeach;?>
                     </select>
-                </div>
+
             </div>
             
             <input type="hidden" value="<?= $id; ?>">
-            <div class="form-group">
-                <div class="col-sm-offset-9 col-sm-10">
+
+
                     <?php if(empty($id)){ ?>
                     <input type="submit" value="Créer" class="btn btn-primary"/>
                     <?php }else{ ?>
                     <input type="submit" value="Modifier" class="btn btn-primary"/>
                     <?php } ?>
                     <button type="submit" class="btn btn-danger">Annuler</button>
-                </div>
-            </form>
-        </div>
+
+
+        </form>
+
     </div>
 
 <?php include 'footer.php'?>
